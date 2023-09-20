a20f4ec471282a3ea0275e5d063159afe45a575d
easternmaster <52649510+easternmaster@users.noreply.github.com>
2023-09-20 14:27:30 +1000

    add application.py to record the wav file

---
+++ src/app.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+import sounddevice as sd
+import numpy as np
+import wavio
+import threading
+
+class Recorder:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Recorder")
+
+        self.is_recording = False
+        self.start_btn = tk.Button(self.master, text="Start Recording", command=self.toggle_recording)
+        self.start_btn.pack(pady=20)
+
+        self.samplerate = 44100  # Hertz
+        self.channels = 2
+        self.filename = "output.wav"
+        self.recording_chunks = []
+
+    def toggle_recording(self):
+        if self.is_recording:
+            self.is_recording = False
+            self.start_btn.config(text="Start Recording")
+            self.stop()
+        else:
+            self.is_recording = True
+            self.start_btn.config(text="Stop Recording")
+            threading.Thread(target=self.record).start()
+
+    def record(self):
+        with sd.InputStream(samplerate=self.samplerate, channels=self.channels) as stream:
+            while self.is_recording:
+                audio_chunk, _ = stream.read(self.samplerate)
+                self.recording_chunks.append(audio_chunk)
+
+    def stop(self):
+        self.is_recording = False
+        if self.recording_chunks:
+            full_recording = np.vstack(self.recording_chunks)
+            wavio.write(self.filename, full_recording, self.samplerate, sampwidth=2)
+            self.recording_chunks = []
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    recorder = Recorder(root)
+    root.mainloop()
+
+
