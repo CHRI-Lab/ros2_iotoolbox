27845233819bbc02d1ab91fb9d8e7e36f603d6c1
easternmaster <52649510+easternmaster@users.noreply.github.com>
2023-10-04 17:31:31 +1100

    Update t_analysis.py

--- src/t_analysis.py
+++ src/t_analysis.py
@@ -15,21 +15,23 @@ class TranslateNode(Node):
         sentence = msg.data
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"Translate the sentence into action and object, for example a sentence 'move that cup' should be 'Action: move; Object: cup'. Now please translate {sentence}",
+            prompt=f"Translate the sentence into action, object and location, for example a sentence 'slowly move that red cup to the top of the table' should be 'Action: slowly move; Object: red cup; Location: top of the table'. Now please translate {sentence}",
             max_tokens=100
         )
 
         text = response['choices'][0]['text']
         action = text.split("Action: ")[1].split(";")[0].strip()
-        obj = text.split("Object: ")[1].strip().replace(".", "")
+        obj = text.split("Object: ")[1].split(";")[0].strip()
+        location = text.split("Location: ")[1].strip().replace(".", "")
 
         self.get_logger().info(f"Action: {action}")
         self.get_logger().info(f"Object: {obj}")
+        self.get_logger().info(f"Location: {location}")
         cost = response['usage']['total_tokens']
         self.get_logger().info(f"Cost would be {cost}")
 
         translated_msg = String()
-        translated_msg.data = f"Action: {action}, Object: {obj}"
+        translated_msg.data = f"Action: {action}, Object: {obj}, Location: {location}"
         self.publisher_.publish(translated_msg)
 
 def main():
@@ -41,3 +43,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
